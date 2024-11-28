b98df70393be96262709404815c9fef9ed74d6ba
Diogo <89143867+Diogo1006br@users.noreply.github.com>
2024-11-28 19:52:20 -0300

    fix

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ import django
 from pytest_django.lazy_django import skip_if_no_django
 
 # Configure o módulo de configurações do Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meu_projeto.TCC_API.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "/meu_projeto/TCC_API/settings")
 
 # Inicialize o Django
 skip_if_no_django()
