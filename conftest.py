cd3cd2a8b387cbe2657d585ea735e82f9269eddd
Diogo <89143867+Diogo1006br@users.noreply.github.com>
2024-12-03 02:19:22 -0300

    Fix confest

--- conftest.py
+++ conftest.py
@@ -3,8 +3,12 @@ import django
 import sys
 from pytest_django.lazy_django import skip_if_no_django
 
+# Adiciona o diretório do projeto ao Python Path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "meu_projeto")))
+
+# Configura o módulo de configurações do Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TCC_API.settings")
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+# Inicializa o Django
 skip_if_no_django()
 django.setup()
-
